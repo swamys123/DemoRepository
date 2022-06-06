716157ce9407bacc3559c899b5473b6f0a1df221
N S Swamys <nsswamys@gmail.com>
2022-06-06 13:34:28 +0530

    For to if

--- Abc.java
+++ Abc.java
@@ -1,7 +1,7 @@
 public class Abc{
 	public static void main(String args[]){
-		for(int i=0;i<5;i++){
-			System.out.println("Just for demo ");
+		if(true){
+			System.out.println("Simply changed ");
 		}
 	}
 }
